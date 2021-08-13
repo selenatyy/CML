1c87056820354cd3f5a109d6702975ffe87e5827
selenatyy_ <selenatyy_@users.noreply.github.com>
2021-08-14 01:13:11 +0800

    Update insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -1,4 +1,121 @@
 use CML;
 
 INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
-values ('1', 'Sylvia Lim','reformation/retop1.jpg','This is a slightly cropped top with a crew neckline and wide straps.','top', 'Tencel','70');
+values ('1', 'Sylvia Lim Ki Chan','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('2', 'Kim Chan hee','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('3', 'Oh Yu Hee','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('4', 'Remus Lim Chee Heng','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('5', 'Rachel Lee Jia En','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('6', 'Nathaniel Michael Yeo','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('7', 'Johnny Neo Ren Jie','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('8', 'Ginny Annabelle Foo','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('9', 'Finn Bolton','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('10', 'Regan Scott','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('11', 'Grace Wise','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('12', 'Haylie Lee Wan Ling','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('13', 'Bailee Kelly','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('14', 'Alex Brooks','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('15', 'Ian Wyatt','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('16', 'Ali Juarez','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('17', 'Nola Ryan','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('18', 'Diego Lynn','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('19', 'Carmen Jacobs','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('20', 'Sebastian Wu Xiao Zhu','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('21', 'Shirley Pacheco','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('22', 'Adriel Landry','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('23', 'Noe Willis','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('24', 'Kash Richards','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('25', 'Paisley Ross','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('26', 'Matthias Horton','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('27', 'Grayson Fisher','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('28', 'Deacon Schwartz','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('29', 'Evan Flynn','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('30', 'Kasey Hebert','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('31', 'Conner Mendez','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('32', 'Tatiana Pham','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('33', 'Athena Beasley','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('34', 'Jase Frederick','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('35', 'Jaylin Murphy','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('36', 'Annabel Fields','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('37', 'Cecilia Dixon','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('38', 'Neil Hester','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('39', 'Chloe Mayer','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('40', 'Paula Phelps','Criminal Defence, Cybercrime','sylvialim123@yahoo.com','4.8', 'English, Mandarin','Sylvia Lim Ki Chan is a Head of the Criminal Defence Department at ABC Law Firm. She is well-versed in many areas but especially CyberCrime and Criminal Defence.');
