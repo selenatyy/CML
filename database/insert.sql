2146ccd9f5550ef6dc5acda1ab4a3cefd0aa14f2
selenatyy_ <selenatyy_@users.noreply.github.com>
2021-08-13 22:47:25 +0800

    Create insert.sql

---
+++ database/insert.sql
@@ -0,0 +1,4 @@
+use CML;
+
+INSERT INTO lawyer ( lawyer_id, lawyer_name, specialisation, lawyer_email, rating, languages, lawyer_description)
+values ('1', 'Sylvia Lim','reformation/retop1.jpg','This is a slightly cropped top with a crew neckline and wide straps.','top', 'Tencel','70');
